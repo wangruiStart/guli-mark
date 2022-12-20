5d4226f74f7a7a7891bd2a17d222e40c77588de9
wangrui <412285675@qq.com>
2022-12-20 16:30:51 +0800

    项目初始结构

---
+++ gulimall-gateway/src/main/java/gulimall/gateway/config/GulimallCorsConfiguration.java
@@ -0,0 +1,28 @@
+package gulimall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+/**
+ * @author wangrui
+ * @Date 2022/12/20 4:08 PM
+ */
+@Configuration
+public class GulimallCorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+        // 1. 配置跨域
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedOrigin("*");
+        configuration.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", configuration);
+        return new CorsWebFilter(source);
+    }
+}
